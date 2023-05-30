24b10d87df5d05967a477baab103c68b6404b7e7
= Daniel <135019810+DanielVerbrugge@users.noreply.github.com>
2023-05-30 13:06:08 +0200

    Update streamlit_app.py
    typos

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,8 @@
 import streamlit
 
-streamlit.title('My parents New Healthy Diner')
+streamlit.title('My Parents New Healthy Diner')
 
-streamlit.header('Breakfast menu')
-streamlit.text('Omega 3 & Blueberry oatmeal')
-streamlit.text('Kale, spinach & rocket smoothy')
-streamlit.text('Har-boiled free-range Egg')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothy')
+streamlit.text('Har-Boiled Free-Range Egg')
