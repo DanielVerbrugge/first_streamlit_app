b35749044e0cd99a6b6d9a49a57ac98e1c0b3861
= Daniel <135019810+DanielVerbrugge@users.noreply.github.com>
2023-05-30 13:05:15 +0200

    Update streamlit_app.py
    Edded breakfast menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,8 @@
 import streamlit
 
-streamlit.title('My parents New healthy Diner')
+streamlit.title('My parents New Healthy Diner')
 
+streamlit.header('Breakfast menu')
+streamlit.text('Omega 3 & Blueberry oatmeal')
+streamlit.text('Kale, spinach & rocket smoothy')
+streamlit.text('Har-boiled free-range Egg')
